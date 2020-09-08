a2517ad38ceb69b3aea997423f7a1f95f739baaa
Anastasiyaaaaaaa <65979372+Anastasiyaaaaaaa@users.noreply.github.com>
2020-09-08 15:13:15 +0300

    build

---
+++ build/precache-manifest.57c1af091c388887e448350a19a28680.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803bbbc96e2c26f621f4cd3d3cb90e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ff47110f3185bced74",
+    "url": "/static/css/main.22b9740b.chunk.css"
+  },
+  {
+    "revision": "fe467753fd1ce1d08ee6",
+    "url": "/static/js/2.c2479459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2479459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ff47110f3185bced74",
+    "url": "/static/js/main.1b66ffb8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f8cb1cae4c4dddae300712deb4783b04",
+    "url": "/static/media/Photo.f8cb1cae.png"
+  },
+  {
+    "revision": "c2160ebe8137c1c51fcc4340559d304a",
+    "url": "/static/media/pattern.c2160ebe.png"
+  }
+]);
