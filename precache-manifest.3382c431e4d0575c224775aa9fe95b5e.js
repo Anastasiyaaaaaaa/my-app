eb0b2c5f708dca421ccdb4c9962b5bb620ee23c9
Anastasiya <soboleva1494@yandex.ru>
2020-09-08 16:21:52 +0300

    Updates

---
+++ precache-manifest.3382c431e4d0575c224775aa9fe95b5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3405058bc62b1bf8d57e1b9ceb43010",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eba6bf20080222edbf7c",
+    "url": "/my-app/static/css/main.9dcd9737.chunk.css"
+  },
+  {
+    "revision": "fe467753fd1ce1d08ee6",
+    "url": "/my-app/static/js/2.c2479459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.c2479459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba6bf20080222edbf7c",
+    "url": "/my-app/static/js/main.1b66ffb8.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "f8cb1cae4c4dddae300712deb4783b04",
+    "url": "/my-app/static/media/Photo.f8cb1cae.png"
+  },
+  {
+    "revision": "c2160ebe8137c1c51fcc4340559d304a",
+    "url": "/my-app/static/media/pattern.c2160ebe.png"
+  }
+]);
